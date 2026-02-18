4079fd86160b401269d474189ddd85c535c2e7d4
teddinata <teddinataa@gmail.com>
2026-02-18 11:34:53 +0700

    fix auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/telescope-login', function () {
+    return view('telescope-login');
+});
+
+Route::post('/telescope-login', function (Request $request) {
+    if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+        return redirect('/telescope');
+    }
+    return back()->withErrors(['email' => 'Invalid credentials']);
 });
